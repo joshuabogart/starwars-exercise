dbc9b5d0a8f3e80000899484fd22133dd67d5bc5
Joshua <joshua.bogart.ndt@gmail.com>
2020-06-30 20:41:22 -0500

    added port

--- server3.js
+++ server3.js
@@ -3,7 +3,7 @@
 var express = require("express");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Data
 // ===========================================================
